150c15020af98bf7d4f731a2d24e0b07a4b0b1a6
bomibear <bomibearly@gmail.com>
2019-09-02 13:31:18 -0700

    first commit

---
+++ src/main/java/com/bomibear/fellowship/FellowshipApplication.java
@@ -0,0 +1,13 @@
+package com.bomibear.fellowship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FellowshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FellowshipApplication.class, args);
+	}
+
+}
